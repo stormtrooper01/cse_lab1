92cf0794f5f3efb80fda3378522a6b2455e5bbb5
Yelena <lena.skripnik.01@gmail.com>
2020-09-30 19:45:42 +0300

    Create server.go

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "net/http"
+    "time" //getting current time
+    "encoding/json"
+)
+
+func main () {
+    http.ListenAndServe(":8795", nil)
+    http.HandleFunc("/", getTime)
+}
+
+func getTime (w http.ResponseWriter, r *http.Request) {
+
+    res := indexRes{time.Now().Format(time.RFC3339)}
+    jsonRes, err := json.Marshal(res)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusAccepted)
+    w.Write(jsonRes)
+}
+
+type indexRes struct {
+    Time string `json:"time"`
+}
