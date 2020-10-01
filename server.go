4c03fb4c5f85fa26210fc846a5690b0d7bb420eb
Yelena <lena.skripnik.01@gmail.com>
2020-10-01 11:02:45 +0300

    Update server.go

--- server.go
+++ server.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import (
@@ -8,8 +7,8 @@ import (
 )
 
 func main () { //this defines the main function
-    http.ListenAndServe(":8795", nil)
-    http.HandleFunc("/", getTime)
+    http.ListenAndServe(":8795", nil)//start the server at port 8795 and pass the router
+    http.HandleFunc("/", getTime)if we have a request in the “/” route, you invoke the getTime()
 }
 
 func getTime (w http.ResponseWriter, r *http.Request) {
@@ -29,35 +28,3 @@ func getTime (w http.ResponseWriter, r *http.Request) {
 type indexRes struct {
     Time string `json:"time"`
 }
-=======
-package main
-
-import (
-    "net/http" //to create the server
-    "time" //getting current time
-    "encoding/json"
-)
-
-func main () {
-    http.ListenAndServe(":8795", nil) //start the server at port 8795 and pass the router
-    http.HandleFunc("/", getTime) //if we have a request in the “/” route, you invoke the getTime()
-}
-
-func getTime (w http.ResponseWriter, r *http.Request) {
-
-    res := indexRes{time.Now().Format(time.RFC3339)}
-    jsonRes, err := json.Marshal(res)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusAccepted)
-    w.Write(jsonRes)
-}
-
-type indexRes struct {
-    Time string `json:"time"`
-}
->>>>>>> comments
